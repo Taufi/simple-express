1b12754cb1f8c49f853a0aaf9cefc9a02ae1cc16
Klaus Dresbach <kd@klausdresbach.de>
2020-06-21 12:52:29 +0200

    small changes

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 app.listen(8080, () => {
-  console.log("TW's server running on port 8080");
+  console.log("Taufi's erver running on port 8080");
 });
 
 app.get("/health", (req, res, next) => {
-  res.status(200).send("Hallo TW!");
+  res.status(200).send("Hallo Taufi!");
 });
 
